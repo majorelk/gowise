e5abd94300b177fdd8e7ac70d3c23927c8b76a07
majorelk <danielevans395@gmail.com>
2023-11-08 00:23:30 +0000

    moved failing tests to separate file - assertions

--- pkg/assertions/assertions_passing_tests.go
+++ pkg/assertions/assertions_passing_tests.go
@@ -122,36 +122,3 @@ func TestAssertions(t *testing.T) {
 	})
 }
 
-// TestAssertions_Failing contains intentionally failing tests.
-// +build failing_tests
-func TestAssertions_Failing(t *testing.T) {
-	t.Run("Equal", func(t *testing.T) {
-		assert := New(t)
-
-		// Test cases
-		testCases := []struct {
-			expected, actual interface{}
-			pass             bool
-		}{
-			{42, 42, true},
-			{42, 23, false},
-			{"hello", "world", false},
-			{true, true, true},
-			{false, true, false},
-		}
-
-		for i, tc := range testCases {
-			t.Run(fmt.Sprintf("Test case %d", i+1), func(t *testing.T) {
-				assert.Equal(tc.expected, tc.actual)
-
-				if tc.pass && assert.Error() != "" {
-					t.Errorf("Test case %d failed, expected no error but got: %s", i+1, assert.Error())
-				} else if !tc.pass && assert.Error() == "" {
-					t.Errorf("Test case %d failed, expected an error but got none", i+1)
-				}
-			})
-		}
-	})
-
-	// Add more intentionally failing tests here
-}
