fca63bf7eff05edde9ca7af10f42fc22a0651d73
majorelk <danielevans395@gmail.com>
2023-11-08 01:15:18 +0000

    added more cases for intentional failing tests to failing file for assertions

--- pkg/assertions/assertions_failing_test.go
+++ pkg/assertions/assertions_failing_test.go
@@ -13,10 +13,8 @@ func TestAssertions_Failing(t *testing.T) {
 			expected, actual interface{}
 			pass             bool
 		}{
-			{42, 42, true},
 			{42, 23, false},
 			{"hello", "world", false},
-			{true, true, true},
 			{false, true, false},
 		}
 
@@ -33,5 +31,81 @@ func TestAssertions_Failing(t *testing.T) {
 		}
 	})
 
-	// Add more intentionally failing tests here
+	t.Run("NotEqual", func(t *testing.T) {
+		assert := New(t)
+
+		// Test cases for NotEqual assertion
+		testCases := []struct {
+			expected, actual interface{}
+			pass             bool
+		}{
+			{42, 42, false},
+			// Add more test cases where expected and actual values are intentionally equal
+		}
+
+		for i, tc := range testCases {
+			t.Run(fmt.Sprintf("Test case %d", i+1), func(t *testing.T) {
+				assert.NotEqual(tc.expected, tc.actual)
+
+				if tc.pass && assert.Error() != "" {
+					t.Errorf("Test case %d failed, expected no error but got: %s", i+1, assert.Error())
+				} else if !tc.pass && assert.Error() == "" {
+					t.Errorf("Test case %d failed, expected an error but got none", i+1)
+				}
+			})
+		}
+	})
+
+	t.Run("True", func(t *testing.T) {
+		assert := New(t)
+
+		// Test cases for True assertion
+		testCases := []struct {
+			value bool
+			pass  bool
+		}{
+			{true, true},
+			{false, false},
+			// Add more test cases where assertion should fail
+		}
+
+		for i, tc := range testCases {
+			t.Run(fmt.Sprintf("Test case %d", i+1), func(t *testing.T) {
+				assert.True(tc.value)
+
+				if tc.pass && assert.Error() != "" {
+					t.Errorf("Test case %d failed, expected no error but got: %s", i+1, assert.Error())
+				} else if !tc.pass && assert.Error() == "" {
+					t.Errorf("Test case %d failed, expected an error but got none", i+1)
+				}
+			})
+		}
+	})
+
+	t.Run("False", func(t *testing.T) {
+		assert := New(t)
+
+		// Test cases for False assertion
+		testCases := []struct {
+			value bool
+			pass  bool
+		}{
+			{true, true},
+			{false, false},
+			// Add more test cases where assertion should fail
+		}
+
+		for i, tc := range testCases {
+			t.Run(fmt.Sprintf("Test case %d", i+1), func(t *testing.T) {
+				assert.False(tc.value)
+
+				if tc.pass && assert.Error() != "" {
+					t.Errorf("Test case %d failed, expected no error but got: %s", i+1, assert.Error())
+				} else if !tc.pass and assert.Error() == "" {
+					t.Errorf("Test case %d failed, expected an error but got none", i+1)
+				}
+			})
+		}
+	})
 }
+
